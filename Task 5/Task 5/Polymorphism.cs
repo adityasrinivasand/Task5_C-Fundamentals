0699a64492d9e34ab179840941c4ff3b93ee1248
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-08-07 16:26:21 +0530

    Create Polymorphism.cs

---
+++ Task 5/Task 5/Polymorphism.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Polymorphism
+    {
+        public void print(int i)
+        {
+            Console.WriteLine($"Printing Int: {i}");
+        }
+        public void print(double f)
+        {
+            Console.WriteLine($"Printing Float: {f}");
+        }
+        public void print(string s)
+        {
+            Console.WriteLine($"Printing String: {s}");
+        }
+    }
+}

690d8033513f4f40caa69ba2326ec831f13ea4a1
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-08-07 16:25:49 +0530

    Create Encapsulation.cs

---
+++ Task 5/Task 5/Encapsulation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Encapsulation
+    {
+        private string empName;
+        private int empID;
+        private float currPay;
+        
+        public Encapsulation() {}
+        public Encapsulation(string name, int id, float pay)
+        {
+            empName = name;
+            empID = id;
+            currPay = pay;
+        }
+        public void GiveBonus(float amount)  
+        {
+            currPay += amount;
+        }
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", empName);
+            Console.WriteLine("ID: {0}", empID);
+            Console.WriteLine("Pay: {0}", currPay);
+        }
+    }
+}

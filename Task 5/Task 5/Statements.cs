a654a5aa468bba407f504a3ae64156ba21f64ae4
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-08-07 16:26:31 +0530

    Create Statements.cs

---
+++ Task 5/Task 5/Statements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task_5
+{
+    class Statements
+    {
+        public void messages()
+        {
+     
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+    }
+}

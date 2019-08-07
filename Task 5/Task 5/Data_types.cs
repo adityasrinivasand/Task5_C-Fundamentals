bccd4ced701f6292351de505a0209ee391d623ac
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-08-07 16:25:36 +0530

    Create Data_types.cs

---
+++ Task 5/Task 5/Data_types.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    public class Data
+    {      
+            string stringvar = "Hello World!!";
+            int intvar = 100;
+            float floatvar = 10.2f;
+            char charvar = 'A';
+            bool boolvar = true;        
+
+        public void disdata()
+        {
+            Console.WriteLine($"String Data Type:-  {stringvar}");
+            Console.WriteLine($"Integer Data Type:-  {intvar}");
+            Console.WriteLine($"Float Data Type:-  {floatvar}");
+            Console.WriteLine($"Character Data Type:-  {charvar}");
+            Console.WriteLine($"Boolen:-  {boolvar}");
+        }
+
+    }
+
+}

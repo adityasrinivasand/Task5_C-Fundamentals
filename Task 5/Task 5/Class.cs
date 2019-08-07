8d0c052298c4f5c94681755e5044400740dbcde9
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-08-07 16:25:27 +0530

    Create Class.cs

---
+++ Task 5/Task 5/Class.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    
+        public class CustomClass
+        {
+            public int Number { get; set; }
+
+            public int Multiply(int num)
+            {
+                return num * Number;
+            }
+
+            public CustomClass()
+            {
+                Number = 0;
+            }
+        }
+}

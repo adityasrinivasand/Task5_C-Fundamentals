fa0baa7907363d49f88bd7e72a33d1251c6438ff
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-08-07 16:26:26 +0530

    Create Properties.cs

---
+++ Task 5/Task 5/Properties.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Properties
+    {
+        private string name = "not known";
+        private int age = 0;
+
+        // Declare a Name property of type string:
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        // Declare a Age property of type int:
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+        public override string ToString()
+        {
+            return " Name = " + Name + ", Age = " + Age;
+        }
+    }
+}

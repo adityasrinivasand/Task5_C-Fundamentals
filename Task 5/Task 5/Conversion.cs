2bd14f2ebcec90c63f5d2cbc251327bb59ee836f
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-08-07 16:25:33 +0530

    Create Conversion.cs

---
+++ Task 5/Task 5/Conversion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    public class ImplicitConversion
+    {
+        int i = 12;
+        double d = 765.12;
+        private float f = 56.123F;
+
+        // Using Built- In Type Conversion 
+        // Methods & Displaying Result 
+        public void displayic()
+        {
+            Console.WriteLine(Convert.ToString(f));
+            Console.WriteLine(Convert.ToInt32(d));
+            Console.WriteLine(Convert.ToUInt32(f));
+            Console.WriteLine(Convert.ToDouble(i));
+        }          
+    }
+}

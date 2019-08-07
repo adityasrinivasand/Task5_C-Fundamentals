aa37f3ce9e9be2facda6f686214a63382bc726a8
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-08-07 16:26:13 +0530

    Create Operator_Overloading.cs

---
+++ Task 5/Task 5/Operator_Overloading.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Operator
+    {
+        private double maths;   
+        private double science;  
+
+        public double Gettotalmarks()
+        {
+            return maths + science;
+        }
+        public void Setmaths(double mat)
+        {
+            maths = mat;
+        }
+        public void Setscience(double sci)
+        {
+            science = sci;
+        }
+
+        // Overload + operator to add two Box objects.
+        public static Operator operator +(Operator b, Operator c)
+        {
+            Operator op = new Operator();
+            op.maths = b.maths + c.maths;
+            op.science = b.science + c.science;
+            return op;
+        }
+    }
+}

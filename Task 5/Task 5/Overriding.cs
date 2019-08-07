2c62b1f4b792c9cc22801eea6269a4304eb29422
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-08-07 16:26:19 +0530

    Create Overriding.cs

---
+++ Task 5/Task 5/Overriding.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Overridingbase
+    {
+        public void show()
+        {
+            Console.WriteLine("Base class");
+        }
+    }
+    class Overridingderived : Overridingbase
+    {
+        new public void show()
+        {
+            Console.WriteLine("Derived class");
+        }
+    }
+}

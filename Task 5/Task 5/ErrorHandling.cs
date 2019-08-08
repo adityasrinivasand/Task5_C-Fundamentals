4b5faa5e568c052a28cb09cc02f241a199737f89
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-08-08 14:49:44 +0530

    Create ErrorHandling.cs

---
+++ Task 5/Task 5/ErrorHandling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class ErrorHandling
+    {
+        public void display()
+        {
+            Console.WriteLine("Input your name");
+            var name = Console.ReadLine();
+            while (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Name can't be empty! Input your name once more");
+                Console.WriteLine("Input your name");
+                name = Console.ReadLine();
+            }
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}

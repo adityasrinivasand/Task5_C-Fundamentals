78992c346f37cff6b9cfd0aa602ec17e50df6ac4
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-08-08 14:48:33 +0530

    Create Base.cs

---
+++ Task 5/Task 5/Base.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Base
+    {
+        public string name;
+        public string subject;
+        public void readers(string name, string subject)
+        {
+            this.name = name;
+            this.subject = subject;
+            Console.WriteLine("Myself: " + name);
+            Console.WriteLine("My Favorite Subject is: " + subject);
+        }
+    }
+
+    class Derived : Base
+    {
+        public Derived()
+        {
+            Console.WriteLine("Derived Class");
+        }
+    }
+}

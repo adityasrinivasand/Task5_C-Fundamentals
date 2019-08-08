501493837185dee2a8a338bf8c00ebb63af321c7
adityasrinivasand <52542602+adityasrinivasand@users.noreply.github.com>
2019-08-08 14:50:12 +0530

    Create Static.cs

---
+++ Task 5/Task 5/Static.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Static
+    {
+        static public int eng = 50;
+        public int maths = 100;
+
+        // Creating static method 
+        // Using static keyword 
+        public static void total()
+        {
+            Console.WriteLine($"Marks in English is: {eng}");
+        }
+        public void mathsmarks()
+        {
+            Console.WriteLine($"Marks in Maths is: {maths}");
+        }
+    }
+}
